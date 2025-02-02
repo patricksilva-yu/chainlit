cc5587ac558f16d04091758e03e068d59d3226a9
patricksilva-yu <patslva@my.yorku.ca>
2025-02-01 21:59:16 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from openai import AsyncOpenAI
+import chainlit as cl
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = AsyncOpenAI(
+    base_url="https://api.studio.nebius.ai/v1/",
+    api_key=os.getenv("API_KEY")
+)
+
+# Instrument the OpenAI client
+cl.instrument_openai()
+
+settings = {
+    "model": "deepseek-ai/DeepSeek-R1",
+    "temperature": 0,
+    # ... more settings
+}
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    response = await client.chat.completions.create(
+        messages=[
+            {
+                "content": "deepseek-ai/DeepSeek-R1",
+                "role": "system"
+            },
+            {
+                "content": message.content,
+                "role": "user"
+            }
+        ],
+        **settings
+    )
+    await cl.Message(content=response.choices[0].message.content).send()
